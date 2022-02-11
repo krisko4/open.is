242315ed59552c61df29d7bd76f9b56663aca383
Krzysztof Wyszyński <wukrzychu@gmail.com>
2022-02-11 19:11:09 +0100

    opening hours almost finished

--- client/src/components/Panel/MainContent/PlaceManagement/PlaceBoard/OpeningHours/OpeningHours.tsx
+++ client/src/components/Panel/MainContent/PlaceManagement/PlaceBoard/OpeningHours/OpeningHours.tsx
@@ -1,91 +1,68 @@
-import { Alert, AlertTitle, Card, CardContent, Grid, Slide, Tab, Tabs, Typography } from "@mui/material"
-import { FC, useState } from "react"
+import { Alert, AlertTitle, Button, Card, CardActions, CardContent, Fade, FormControlLabel, FormGroup, Grid, Slide, Switch, Tab, Tabs, Toolbar, Tooltip, Typography } from "@mui/material"
+import { FC, useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { useCurrentPlaceContext } from "../../../../../../contexts/PanelContexts/CurrentPlaceContext"
-import { OpeningHoursCard } from "../../../../../reusable/OpeningHours/OpeningHoursCard"
-import { OpeningHoursForm } from "../../../../../reusable/OpeningHours/OpeningHoursForm"
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import frLocale from 'date-fns/locale/fr';
-
-import StaticTimePicker from '@mui/lab/StaticTimePicker';
 import { SingleDayOpeningHours } from "./SingleDayOpeningHours"
+
 export const OpeningHours: FC = () => {
 
-    const { currentPlace } = useCurrentPlaceContext()
-    const [startHour, setStartHour] = useState<any>()
-    const [endHour, setEndHour] = useState<any>()
+    const { setCurrentPlace, currentPlace } = useCurrentPlaceContext()
 
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState('monday');
+    const [areHoursValid, setHoursValid] = useState(false)
 
-    const { openingHours } = currentPlace
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const [openingHours, setOpeningHours] = useState<any>(
+        {
+            monday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            tuesday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            wednesday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            thursday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            friday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            saturday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            },
+            sunday: {
+                start: '8:00',
+                end: '16:00',
+                valid: true
+            }
+        }
+    )
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue)
-        setStartHour(schedule[newValue].start)
-        setEndHour(schedule[newValue].end)
     }
 
-    const schedule = openingHours ? [
-        {
-            start: openingHours.monday.startHour,
-            end: openingHours.monday.endHour,
-        },
-        {
-            start: openingHours.tuesday.startHour,
-            end: openingHours.tuesday.endHour
-        },
-        {
-            start: openingHours.wednesday.startHour,
-            end: openingHours.wednesday.endHour
-        },
-        {
-            start: openingHours.thursday.startHour,
-            end: openingHours.thursday.endHour
-        },
-        {
-            start: openingHours.friday.startHour,
-            end: openingHours.friday.endHour
-        },
-        {
-            start: openingHours.saturday.startHour,
-            end: openingHours.saturday.endHour
-        },
-        {
-            start: openingHours.sunday.startHour,
-            end: openingHours.sunday.endHour
-        },
+    useEffect(() => {
+        if (Object.keys(openingHours).some(day => !openingHours[day].valid)) {
+            setHoursValid(false)
+            return
+        }
+        setHoursValid(true)
 
-    ] : [
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-        {
-            start: '8:00',
-            end: '16:00'
-        },
-    ]
+    }, [openingHours])
 
     const days = [
         'Monday',
@@ -99,59 +76,77 @@ export const OpeningHours: FC = () => {
     ]
 
     return (
-        <Grid container sx={{ height: '100%' }} alignItems="center">
-            <Grid container justifyContent="space-evenly">
-                <Grid container item lg={11} sx={{mb: 5}}>
-                    <Alert variant="filled" sx={{flexGrow: 1}} severity="error">
-                        SIemanko
-                    </Alert>
-                </Grid>
-                <Grid item lg={6}>
-                    <Card sx={{ height: '100%' }}>
-                        <Grid container direction="column" sx={{ height: '100%' }}>
-                            <Tabs value={value} onChange={handleChange} variant="fullWidth" sx={{ width: '100%' }}>
-                                {days.map((day) =>
-                                    <Tab label={day} key={day} />
-                                )}
-                            </Tabs>
-                            <SingleDayOpeningHours
-                                startHour={startHour}
-                                setStartHour={setStartHour}
-                                endHour={endHour}
-                                setEndHour={setEndHour}
-                            />
-                        </Grid>
-                    </Card>
-                </Grid>
-                <Grid item lg={5}>
-                    <Slide in={true} timeout={1000} direction="left">
-                        <Card>
-                            <CardContent>
-                                <Typography variant="h2" style={{ color: 'white' }}>Opening hours management</Typography>
-                                <Grid style={{ marginTop: 10 }} item lg={11}>
-                                    <Typography variant="body1">
-                                        This is a crucial section of your business.
-                                        Our great mission is to encourage you to update the opening state of your place
-                                        as frequently as it is possible. Your commitment will shortly result with benefits:
-                                    </Typography>
-                                </Grid>
-                                <Alert sx={{ mt: 3, mb: 1 }} variant="outlined" >
-                                    <AlertTitle>Information</AlertTitle>
-                                    Your visitors will stay up to date with your opening hours
-                                </Alert>
-                                <Alert sx={{ mb: 1 }} variant="outlined" >
-                                    <AlertTitle>Reliability</AlertTitle>
-                                    Your business will be considered as reliable
-                                </Alert>
-                                <Alert variant="outlined" >
-                                    <AlertTitle>Growth</AlertTitle>
-                                    Your customer basis will grow
-                                </Alert>
-                            </CardContent>
-                        </Card>
-                    </Slide>
+        <Grid container sx={{ height: '100%', overflow: 'hidden' }} direction="column">
+            {currentPlace.isActive ||
+                <Alert variant="filled" severity="warning">
+                    Your place is currently not visible in the browser. Please set opening hours to activate your business.
+                </Alert>
+            }
+            <Grid sx={{ flexGrow: 1 }} container alignItems="center">
+                <Grid container justifyContent="space-evenly">
+                    <Grid item lg={6}>
+                        <Fade in={true} timeout={1000}>
+                            <Card sx={{ height: '100%' }}>
+                                <Grid container direction="column" sx={{ height: '100%' }}>
+                                    <Tabs value={value} onChange={handleChange} variant="fullWidth" sx={{ width: '100%' }}>
+                                        {days.map((day) =>
+                                            <Tab value={day.toLowerCase()} label={day} key={day} />
+                                        )}
+                                    </Tabs>
+                                    <SingleDayOpeningHours
+                                        openingHours={openingHours}
+                                        setOpeningHours={setOpeningHours}
+                                        day={value.toLowerCase()}
+                                    />
 
+                                </Grid>
+                            </Card>
+                        </Fade>
+                    </Grid>
+                    <Grid item lg={5}>
+                        <Slide in={true} timeout={1000} direction="left">
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="h2" style={{ color: 'white' }}>Opening hours management</Typography>
+                                    <Grid style={{ marginTop: 10 }} item lg={11}>
+                                        <Typography variant="body1">
+                                            This is a crucial section of your business.
+                                            Our great mission is to encourage you to update the opening state of your place
+                                            as frequently as it is possible. Your commitment will shortly result with benefits:
+                                        </Typography>
+                                    </Grid>
+                                    <Alert sx={{ mt: 3, mb: 1 }} variant="outlined" >
+                                        <AlertTitle>Information</AlertTitle>
+                                        Your visitors will stay up to date with your opening hours
+                                    </Alert>
+                                    <Alert sx={{ mb: 1 }} variant="outlined" >
+                                        <AlertTitle>Reliability</AlertTitle>
+                                        Your business will be considered as reliable
+                                    </Alert>
+                                    <Alert variant="outlined" >
+                                        <AlertTitle>Growth</AlertTitle>
+                                        Your customer basis will expand
+                                    </Alert>
+                                </CardContent>
+                            </Card>
+                        </Slide>
+
+                    </Grid>
                 </Grid>
+            </Grid>
+            <Grid
+                container
+                sx={{
+                    backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09))'
+                }}
+            >
+                <Toolbar sx={{ flexGrow: 1 }}>
+                    <Grid container justifyContent="flex-end">
+                        <Tooltip title="Save your opening hours">
+                            <Button variant="contained" disabled={!areHoursValid} size="large" color="primary">Save changes</Button>
+                        </Tooltip>
+                    </Grid>
+                </Toolbar>
             </Grid>
         </Grid>
     )
--- client/src/components/Panel/MainContent/PlaceManagement/PlaceBoard/OpeningHours/SingleDayOpeningHours.tsx
+++ client/src/components/Panel/MainContent/PlaceManagement/PlaceBoard/OpeningHours/SingleDayOpeningHours.tsx
@@ -7,27 +7,66 @@ import frLocale from 'date-fns/locale/fr';
 
 import StaticTimePicker from '@mui/lab/StaticTimePicker';
 import { useCurrentPlaceContext } from "../../../../../../contexts/PanelContexts/CurrentPlaceContext";
-import { Grid } from "@mui/material";
-import { FC, useState , useEffect} from "react";
+import { Alert, FormControlLabel, FormGroup, Grid, Switch } from "@mui/material";
+import { FC, useState, useEffect, useRef } from "react";
+import { getHours, isAfter } from "date-fns";
 
-export const SingleDayOpeningHours: FC<any> = ({startHour, endHour, setStartHour, setEndHour}) => {
+interface Props {
+    // day: 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday',
+    day: string,
+    openingHours: any,
+    setOpeningHours: any
+
+}
+
+export const SingleDayOpeningHours: FC<Props> = ({ day, openingHours, setOpeningHours }) => {
 
     const { currentPlace } = useCurrentPlaceContext()
+    const [startHour, setStartHour] = useState<any>()
+    const [endHour, setEndHour] = useState<any>()
+    const [areHoursValid, setHoursValid] = useState(true)
+    const isNextDay = useRef(false)
+
+
 
     useEffect(() => {
         console.log(startHour)
+        let valid
+        if (!isAfter(new Date(endHour), new Date(startHour)) && getHours(new Date(endHour)) > 6) {
+            valid = false
+        } else {
+            valid = true
+        }
+        setHoursValid(valid)
+        const newOpeningHours = { ...openingHours }
+        newOpeningHours[day].start = startHour
+        newOpeningHours[day].valid = valid
+        newOpeningHours[day].end = endHour
+        setOpeningHours(newOpeningHours)
+    }, [startHour, endHour])
 
-    }, [startHour])
+    useEffect(() => {
+        setStartHour(openingHours[day].start)
+        setEndHour(openingHours[day].end)
+    }, [day])
+
 
     return (
         <Grid container sx={{ flexGrow: 1 }} alignItems="center" justifyContent="space-evenly">
+            {
+                areHoursValid ||
+                <Grid container lg={10}>
+                    <Alert severity="error" variant="outlined" sx={{ flexGrow: 1 }}>
+                        The difference between opening and closing hour is invalid
+                    </Alert>
+                </Grid>
+            }
             <LocalizationProvider locale={frLocale} dateAdapter={AdapterDateFns}>
                 <StaticTimePicker
                     toolbarTitle="Opening hour"
                     displayStaticWrapperAs="mobile"
                     value={startHour}
                     onChange={(newValue) => {
-                        console.log(newValue)
                         setStartHour(newValue)
                     }}
                     renderInput={(params) => <TextField {...params} />}
